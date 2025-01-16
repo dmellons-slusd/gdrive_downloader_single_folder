1bda40bf49b6c422c7d6cf3663107c0cf07e1d9f
David Mellons <dmellons@slusd.us>
2025-01-16 08:50:03 -0800

    Updated access and cleanup

--- download_gdrive_images.py
+++ download_gdrive_images.py
@@ -1,27 +1,32 @@
 import gspread
+import json
+import requests
 import gdown
 import shutil
 from os import path
 from oauth2client.service_account import ServiceAccountCredentials
+from icecream import ic
+# from slusdlib import core
 # # Unable to install until Python >= 3.8 is installed
 # from slusdlib.core import read_gsheet
 
-# # Set Sheet URLs of each site sheet here
+# # Set Sheet URLs of each site's core sheet here
 sheet_urls = {
         'slusd':'https://docs.google.com/spreadsheets/d/1lgXm6YQTCR3AHIjVrSDkgvkVX9cwx-2O-taeP_ss2zw/edit?usp=sharing',
-        'garfield': None,
-        'jefferson':'https://docs.google.com/spreadsheets/d/1LYA0097njxt58udNwTohsTJTcGB4fK34o4XOv9B_8qk/edit#gid=0',
-        'madison': None,
-        'mckinley': None,
-        'monrow': None,
-        'roosevelt': None,
-        'washington': None,
-        'wilson': None,
-        'bancroft': None,
-        'muir': None,
-        'lincoln': None,
-        'slhs': None,
-        'adult': None
+        'garfield': 'https://docs.google.com/spreadsheets/d/1helB1s9Ffw55kunhsE7YVqxQXTsK98c1u4KQz6zL_dw/edit?usp=sharing',
+        'jefferson':'https://docs.google.com/spreadsheets/d/1LYA0097njxt58udNwTohsTJTcGB4fK34o4XOv9B_8qk/edit?usp=sharing',
+        'madison': 'https://docs.google.com/spreadsheets/d/1rv9istdWf97vKupj7nqrc81rR6Bz4_EsicNw8AxsCFk/edit?usp=sharing',
+        'mckinley': 'https://docs.google.com/spreadsheets/d/17a6eMTRKBDODcZHjNqgTBeiw9mVYXsFWPENwYx5T360/edit?usp=sharing',
+        'monroe': 'https://docs.google.com/spreadsheets/d/1Kkq0lMLJpGizkKva9lgLiljQDJrF1TRnlaSk2NyxrEg/edit?usp=sharing',
+        'roosevelt': 'https://docs.google.com/spreadsheets/d/1gfqkEjxWpmQVlxDcGubEtAIHZNyU2MlsSudfSPto9Io/edit?usp=sharing',
+        'washington': 'https://docs.google.com/spreadsheets/d/1n093HqhKGnXmUnEhWcsgBwKT1NRwj_6QR0_Z4ABwOXg/edit?usp=sharing',
+        'halkin': 'https://docs.google.com/spreadsheets/d/1KouJ3M5TDdnd0CHsvjQi6RDj6a4kXqvLoexXA55LPCo/edit?usp=sharing',
+        'bancroft': 'https://docs.google.com/spreadsheets/d/1JUrvpPWN7-I0qNPMNlZQtVsMsHQgwtLpbyLco1qDTtg/edit?usp=sharing',
+        'muir': 'https://docs.google.com/spreadsheets/d/1Kkq0lMLJpGizkKva9lgLiljQDJrF1TRnlaSk2NyxrEg/edit#gid=0',
+        'lincoln': 'https://docs.google.com/spreadsheets/d/1hloCf_7G-IEi2fPHKoajc2c6YE2d4vW8YqWL3JVPSjY/edit?usp=sharing',
+        'slhs': 'https://docs.google.com/spreadsheets/d/1dI_GiYOry4M-XTMcuh25uZlC4EXRXWWOR4jHD3nc4Rk/edit?usp=sharing',
+        'adult': 'https://docs.google.com/spreadsheets/d/1_xj63pU1Jg0NTBx0OwfOED4f--QitiYmGCFadaHvfd8/edit?usp=sharing',
+        'slva': 'https://docs.google.com/spreadsheets/d/1GQodWj-qsV4jW-kbXnGAlGHTBWLwRRxm0ySm_DFvBaI/edit?usp=sharing',
     } 
 
 def read_gsheet(sheet_url, credentials_file='creds.json', sheet_name='Sheet1',columns=['name','Folder ID']):
@@ -67,17 +72,32 @@ if __name__ == "__main__":
     gsheets-service@polar-winter-333619.iam.gserviceaccount.com
 
     """
-    
+    creds = json.load(open('creds.json'))
+    ic(creds)
     image_folder_home = '/var/www/html/images'
     for school,sheet_url in sheet_urls.items():
         if sheet_url == None:
             continue
-        print('~~~Sheet Exists~~~~: ' ,sheet_url)
+        
         google_drive_folder_info = read_gsheet(sheet_url,'/home/administrator/gdrive_downloader/creds.json')
         for download_info in google_drive_folder_info:
             folder_url = f"https://drive.google.com/drive/folders/{download_info['Folder ID']}"
             folder_path = f"{image_folder_home}/{school}/{download_info['name']}"
-            if path.exists(folder_path):
-                shutil.rmtree(folder_path)
-            gdown.download_folder(folder_url, output=folder_path, quiet=False)
+            ic(folder_path)
+            if path.exists(folder_path): 
+                ic(f'File {folder_path} already exists')
+                # continue
+                # shutil.rmtree(folder_path)
+            ic(folder_url)
+            try:
+                gdown.download_folder(folder_url, output=folder_path, quiet=False)
+            except Exception as e:
+                ic(e)
+                # core.log(e)
+            # response = requests.get(folder_url, headers={'Authorization': 'Bearer {0}'.format(creds['token'])})
+            # response.raise_for_status()
+
+            # # Write content to file
+            # with open(folder_path, 'wb') as f:
+            #     f.write(response.content)
 
--- download_images.sh
+++ download_images.sh
@@ -1,7 +1,7 @@
 log_file="/home/administrator/gdrive_downloader/log.log"
 start=$(date)
 echo "$start - Started Downloading images" >> $log_file
-python3 /home/administrator/gdrive_downloader/download_gdrive_images.py
+sudo python3 /home/administrator/gdrive_downloader/download_gdrive_images.py
 end=$(date)
 echo "$end - Completed downloading images" >> $log_file
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> $log_file
